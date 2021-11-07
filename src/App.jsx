fed00308f6a41a6a62efa24f40a2e51228f942a4
Jath <61468377+jath-git@users.noreply.github.com>
2021-11-07 17:07:54 -0500

    use dotenv for config

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Chat from "./components/Chat/Chat.jsx";
 import Join from "./components/Join/Join.jsx";
 import Participants from "./components/Participants/Participants.jsx";
 import Password from "./components/Password/Password.jsx";
-
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
@@ -12,14 +11,26 @@ import "firebase/compat/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBRsY3uU6Sxr-tZYXgwDGn-lN4yM58nwU0",
-  authDomain: "chatbox-authentication.firebaseapp.com",
-  projectId: "chatbox-authentication",
-  storageBucket: "chatbox-authentication.appspot.com",
-  messagingSenderId: "408104309600",
-  appId: "1:408104309600:web:3d7a0fc25d88b71d698405",
-  measurementId: "G-Q5FPW0B4MZ",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 });
+
+
+// firebase.initializeApp({
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: "chatbox-authentication.firebaseapp.com",
+//   projectId: "chatbox-authentication",
+//   storageBucket: "chatbox-authentication.appspot.com",
+//   messagingSenderId: "408104309600",
+//   appId: "1:408104309600:web:3d7a0fc25d88b71d698405",
+//   measurementId: "G-Q5FPW0B4MZ",
+// });
+
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
